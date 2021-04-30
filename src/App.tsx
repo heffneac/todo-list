3b21991d33fead6c16ba2d6f8df1ed8d4a617a81
Alec Heffner <aheffner@callibrity.com>
2021-04-29 20:24:11 -0400

    Add Filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@material-ui/core';
 import Checkbox from '@material-ui/core/Checkbox';
+import Chip from "@material-ui/core/Chip";
 import Modal from "@material-ui/core/Modal";
 import Paper from "@material-ui/core/Paper";
 import Table from "@material-ui/core/Table";
@@ -22,8 +23,15 @@ interface FormData {
   name: string;
 }
 
+enum Filter {
+  All = "ALL",
+  Complete = "COMPLETE",
+  Incomplete = "INCOMPLETE",
+}
+
 const App = () => {
   const [items, setItems] = useState<TodoItem[]>([]);
+  const [filter, setFilter] = useState(Filter.All);
   const [open, setOpen] = useState(false);
   const { handleSubmit, register, reset, formState } = useForm<FormData>({
     mode: "onChange",
@@ -43,6 +51,27 @@ const App = () => {
 
   const updateLocalStorage = (items: TodoItem[]) => {
     localStorage.setItem("items", JSON.stringify(items));
+  };
+
+  const updateFilter = (filter: Filter) => {
+    let filteredItems: TodoItem[] = [];
+    switch (filter) {
+      case Filter.All:
+        filteredItems = getItemsFromLocalStorage();
+        break;
+      case Filter.Complete:
+        filteredItems = getItemsFromLocalStorage().filter(
+          (item) => item.completed
+        );
+        break;
+      case Filter.Incomplete:
+        filteredItems = getItemsFromLocalStorage().filter(
+          (item) => !item.completed
+        );
+        break;
+    }
+    setItems(filteredItems);
+    setFilter(filter);
   };
 
   const handleOpen = () => {
@@ -76,6 +105,7 @@ const App = () => {
   }
 
   const updateSearch = async (input: string) => {
+    setFilter(Filter.All);
     if (input.length === 0) {
       loadItemsFromLocalStorage();
     }
@@ -109,42 +139,61 @@ const App = () => {
         </Button>
       </div>
       {items.length > 0 ? (
-        <TableContainer className="table-container" component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell align="center" className="completed">
-                  Completed
-                </TableCell>
-                <TableCell className="name">Name</TableCell>
-                <TableCell className="delete" align="center"></TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {items.map((item, index) => (
-                <TableRow key={index}>
-                  <TableCell align="center" component="th" scope="row">
-                    <Checkbox
-                      checked={item.completed}
-                      onChange={() => markCommpleted(index)}
-                      color="primary"
-                    />
+        <>
+          <div className="filters">
+            <Chip
+              color={filter === Filter.All ? "primary" : undefined}
+              label="All"
+              onClick={() => updateFilter(Filter.All)}
+            />
+            <Chip
+              color={filter === Filter.Complete ? "primary" : undefined}
+              label="Complete"
+              onClick={() => updateFilter(Filter.Complete)}
+            />
+            <Chip
+              color={filter === Filter.Incomplete ? "primary" : undefined}
+              label="Incomplete"
+              onClick={() => updateFilter(Filter.Incomplete)}
+            />
+          </div>
+          <TableContainer className="table-container" component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center" className="completed">
+                    Completed
                   </TableCell>
-                  <TableCell>{item.name}</TableCell>
-                  <TableCell align="center">
-                    <Button
-                      color="secondary"
-                      onClick={() => deleteItem(index)}
-                      variant="contained"
-                    >
-                      Delete
-                    </Button>
-                  </TableCell>
+                  <TableCell className="name">Name</TableCell>
+                  <TableCell className="delete" align="center"></TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                {items.map((item, index) => (
+                  <TableRow key={index}>
+                    <TableCell align="center" component="th" scope="row">
+                      <Checkbox
+                        checked={item.completed}
+                        onChange={() => markCommpleted(index)}
+                        color="primary"
+                      />
+                    </TableCell>
+                    <TableCell>{item.name}</TableCell>
+                    <TableCell align="center">
+                      <Button
+                        color="secondary"
+                        onClick={() => deleteItem(index)}
+                        variant="contained"
+                      >
+                        Delete
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </>
       ) : (
         <div className="no-items">No Items</div>
       )}
@@ -171,10 +220,7 @@ const App = () => {
                 >
                   Add
                 </Button>
-                <Button
-                  onClick={handleClose}
-                  variant="contained"
-                >
+                <Button onClick={handleClose} variant="contained">
                   Cancel
                 </Button>
               </div>
